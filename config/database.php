afd756c52392b34ac70c5d41f4d1683143462521
Mike <ng1ny0mike51@gmail.com>
2026-03-23 10:50:36 +0300

    Final Push

--- config/database.php
+++ config/database.php
@@ -1,17 +1,21 @@
 <?php
 
 class Database {
-    private $host = 'localhost';
-    private $db   = 'kejamtaani';
-    private $user = 'root';
-    private $pass = '';
+    private $host = 'sql307.infinityfree.com'; 
+    private $db   = 'if0_41455095_kejamtaani'; 
+    private $user = 'if0_41455095'; 
+    private $pass = 'a8tmr2LRIGbyu8Z'; 
     private $charset = 'utf8mb4';
-
     public function getConnection() {
         $dsn = "mysql:host=$this->host;dbname=$this->db;charset=$this->charset";
-        return new PDO($dsn, $this->user, $this->pass, [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-        ]);
+        try {
+            return new PDO($dsn, $this->user, $this->pass, [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+            ]);
+        } catch (PDOException $e) {
+            
+            die("Connection failed: " . $e->getMessage());
+        }
     }
 }
